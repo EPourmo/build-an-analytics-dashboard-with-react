2840433ac331af55007e72e94c2808ce232bb99c
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-09-03 18:31:51 +0200

    work on welcome component

--- P12FrontEnd/src/App.jsx
+++ P12FrontEnd/src/App.jsx
@@ -3,6 +3,7 @@ import InfoCards from "./components/InfoCards.jsx";
 import Activities from "./components/Activities.jsx";
 import Sessions from "./components/Sessions.jsx";
 import Performances from "./components/Performances.jsx";
+import Welcome from "./components/Welcome.jsx";
 
 function App() {
   const userId = 18;
@@ -12,7 +13,7 @@ function App() {
       {/* <Activities userId={userId} />
       <Sessions userId={userId} />
       <Performances userId={userId} /> */}
-
+      <Welcome name="Thomas" />
       {/* <InfoCards userId={userId} /> */}
     </div>
   );
--- P12FrontEnd/src/components/Welcome.jsx
+++ P12FrontEnd/src/components/Welcome.jsx
@@ -1,8 +1,10 @@
 export default function Welcome({ name }) {
   return (
-    <div>
-      <h1 className="welcome">{`Bonjour ${name}`}</h1>
-      <p className="congrats">
+    <div className="font-roboto ">
+      <h1 className="font-medium text-5xl mb-10">
+        Bonjour <span className="text-red251">{name}</span>
+      </h1>
+      <p className="font-normal text-lg	">
         Félicitation ! Vous avez explosé vos objectifs hier 👏
       </p>
     </div>
